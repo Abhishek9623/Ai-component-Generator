9102d0446d4d196ab537dffa6264dccbc9340105
X_Abhi <abhishekvaidya8766@gmail.com>
2026-09-14 13:53:56 +0530

    Fix Navbar import

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from '../componant/Navbar.jsx'
+import Navbar from "../componant/Navbar.jsx";
 import Select from 'react-select'
 import { BsStars } from 'react-icons/bs'
 import { HiOutlineCode } from 'react-icons/hi'
